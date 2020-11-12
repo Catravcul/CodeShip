d903664229156764c182ee1e663963c2c7ff6e51
beron <bleink@outlook.es>
2020-11-12 18:20:33 +0100

    src/Spaceship.js - add wing property and render its model in spaceship

--- src/Spaceship.js
+++ src/Spaceship.js
@@ -1,12 +1,14 @@
 import {GltfLoader} from './utils/GltfLoader'
+import {StandardPE} from './propulsion_engines/StandardPE'
 
 export class Spaceship {
     spaceship
-    wingLeft
+    wingLeft = new StandardPE()
     fuselage
-    
+
     constructor(p_spaceship, p_fuselagePath = '3d/Magnate.glb') {
         this.spaceship = p_spaceship
+        this.wingLeft.renderOnGroup(this.spaceship)
         GltfLoader.loadAsChild(this.spaceship, p_fuselagePath, [this,'fuselage'])
     }
 

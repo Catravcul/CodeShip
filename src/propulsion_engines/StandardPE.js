c612ac9881ee8f252de927fd2ff1ea73a3cf844c
beron <bleink@outlook.es>
2020-11-15 17:04:53 +0100

    src/propulsion_engine.js - added middleware & coordinates to align itself in render

--- src/propulsion_engines/StandardPE.js
+++ src/propulsion_engines/StandardPE.js
@@ -1,17 +1,29 @@
 import {GltfLoader} from '../utils/GltfLoader'
 
-export class StandardPE{
+export class StandardPE {
     glbPath = '3d/propulsion_engine/StandardPE.glb'
+    position
     lifePoints
     color
     scene
+    
+    middlewares = {
+        alignSelf(instance, scene) {
+            for (let axis in instance.position) {
+                const distance = instance.position[axis]
+                scene['translate' + axis.toUpperCase()](distance)
+            }
+        }
+    }
 
-    constructor(p_color = 'grey', p_lifePoints = 100) {
+    constructor(p_position, p_color = 'grey', p_lifePoints = 100) {
+        this.position = p_position
         this.lifePoints = p_lifePoints
         this.color = p_color
     }
 
-    renderOnGroup(group, loadAsChild = GltfLoader.loadAsChild){
-        loadAsChild(group, this.glbPath, [this,'scene'])
+    renderOnGroup(group, loadAsChild = GltfLoader.loadAsChild) {
+        loadAsChild.call(this, group, this.glbPath, [this,'scene'], this.middlewares)
     }
+
 }

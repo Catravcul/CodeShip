47b71bc5cfa551be9fec8b7fc4e37d762d47b358
beron <bleink@outlook.es>
2020-11-18 09:22:24 +0100

    src/threeModels/propulsionEngine/[StandardPE, PropulsionEngine].js - added Rotation

--- src/threeModels/propulsionEngines/PropulsionEngine.js
+++ src/threeModels/propulsionEngines/PropulsionEngine.js
@@ -1,11 +1,18 @@
 import { ThreeModel } from '../ThreeModel'
 
 export class PropulsionEngine extends ThreeModel {
+    position
+    rotation
     middlewares = {
         alignSelf(instance, scene) {
             for (let axis in instance.position) {
                 const distance = instance.position[axis]
                 scene['translate' + axis.toUpperCase()](distance)
+            }
+            for (let axis in instance.rotation) {
+                const grades = instance.rotation[axis]
+                console.log(grades)
+                scene['rotate' + axis.toUpperCase()](grades)
             }
         }
     }

6db309044ed96f133461d917f6590c6c622768c1
beron <bleink@outlook.es>
2020-11-18 09:24:15 +0100

    src/threeModels/Components.js - exports all spaceship components

---
+++ src/threeModels/Components.js
@@ -0,0 +1,4 @@
+export { StandardPE } from './propulsionEngines/StandardPE'
+export { StandardF } from './fuselage/StandardF'
+export { MiniPE } from './propulsionEngines/MiniPE'
+export { MiniF } from './fuselage/MiniF'

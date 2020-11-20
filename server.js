81726c2ddf5e33386155896320b4ab7fb0e143f9
beron <bleink@outlook.es>
2020-11-20 15:06:00 +0100

    server.js - changed fs to fs/promises, added body-parser & put route to change Spaceship.js file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const fs = require( 'fs' )
+const fs = require( 'fs/promises' )
 const path = require( 'path' )
+const body_parser = require( 'body-parser' )
 const express = require( 'express' )
 
 const app = express( )
 
+app.use( body_parser.urlencoded( { extended : false } ) )
+app.use( body_parser.json() )
 app.use( express.static( path.join( __dirname, 'build' ) ) );
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
@@ -19,14 +22,29 @@ app.get( '/*', ( req, res ) => {
   res.sendFile( path.join( __dirname + '/build/index.html' ) ) 
 } );
 app.post( '/spaceship/class', ( req, res ) => { 
-    fs.readFile( 'src/threeModels/Spaceship.js', { encoding : 'utf-8' }, ( err, data ) => {
-        const response = {
-            fileText : data,
-            error : err
-        }
-        console.log( typeof data)
+    const response = {}
+    fs.readFile( 'src/threeModels/Spaceship.js', { encoding : 'utf-8' })
+    .then( ( data ) => {
+        response.fileText = data
         res.status( 200 ).json( response )
     } )
-  } );
+    .catch( ( err ) => {
+        response.error = err
+        res.status( 500 ).json( response )
+    } )
+} );
+app.put( '/spaceship/class', ( req, res ) => {
+    fs.writeFile( 'src/threeModels/Spaceship.js', req.body.fileText )
+    .then( () => {
+        response.message = 'well done'
+        res.status( 200 ).json( response )
+    } )
+    .catch( err => {
+        const response = {
+            error : err
+        }
+        res.status( 500 ).json( response )
+    } )
+} )
 
 app.listen( 300 )

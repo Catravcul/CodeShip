d98a11b3dfc82bc2f8a0bcff2f659796c76ccc44
beron <bleink@outlook.es>
2020-11-24 22:14:07 +0100

    erased threeModels, Materials, utils, game.js from src, 'cause they now belong public

--- src/materials/Cristal.js
+++
@@ -1,9 +0,0 @@
-import { Material } from './Material'
-
-export class Cristal extends Material {
-    constructor() {
-        super()
-        this.material = this.MeshPhongMaterial.clone()
-        this.material.opacity = 0.3
-    }
-}
--- src/threeModels/Components.js
+++
@@ -1,6 +0,0 @@
-export { StandardPE } from './propulsionEngines/StandardPE'
-export { StandardF } from './fuselage/StandardF'
-export { MiniPE } from './propulsionEngines/MiniPE'
-export { MiniF } from './fuselage/MiniF'
-export { BirdPE } from './propulsionEngines/BirdPE'
-export { BirdF } from './fuselage/BirdF'

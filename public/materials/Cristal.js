70b629506a9a149f96cf1299466322d5a879d0d8
beron <bleink@outlook.es>
2020-12-16 12:18:13 +0100

    deleted [materials, threeModels, util] folders, .gitignore, package.json, script with scene in index.html

--- public/materials/Cristal.js
+++
@@ -1,9 +0,0 @@
-import { Material } from './Material.js'
-
-export class Cristal extends Material {
-    constructor() {
-        super()
-        this.material = this.MeshPhongMaterial.clone()
-        this.material.opacity = 0.3
-    }
-}

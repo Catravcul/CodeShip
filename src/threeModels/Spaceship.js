754cfd41430bff9174d49336ab6181f16cb78590
beron <bleink@outlook.es>
2020-12-19 20:57:00 +0100

    Spaceship.js - add components variable to save Components name for renderComponents func

--- src/threeModels/Spaceship.js
+++ src/threeModels/Spaceship.js
@@ -6,8 +6,17 @@ export class Spaceship {
     takeoff = new Components.StandardTO(this.fuselage.positionTO, this.fuselage.rotationTO, this.fuselage.scaleTO)
     propulsionEngine = new Components.BirdPE(this.fuselage.positionPE, this.fuselage.rotationPE)
 
+    components = {fuselage: 'StandardF', takeoff: 'StandardTO', propulsionEngine: 'StandardPE'}
+
     constructor(p_spaceship) {
         this.spaceship = p_spaceship
+        this.renderComponents()
+    }
+
+    renderComponents() {
+        this.fuselage = new Components[this.components['fuselage']]()
+        this.takeoff = new Components[this.components['takeoff']](this.fuselage.positionTO, this.fuselage.rotationTO, this.fuselage.scaleTO)
+        this.propulsionEngine = new Components[this.components['propulsionEngine']](this.fuselage.positionPE, this.fuselage.rotationPE)
         this.fuselage.renderOnGroup(this.spaceship)
         this.takeoff.renderOnGroup(this.spaceship)
         this.propulsionEngine.renderOnGroup(this.spaceship)

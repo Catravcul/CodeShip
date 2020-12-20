da8b06bc4458a8784f261fe86ba9590eeb5e31b7
beron <bleink@outlook.es>
2020-12-20 15:34:53 +0100

    nav.js - change state to customize, add buttons to apply & cancel changes to spaceship

--- src/UserInterface/nav.js
+++ src/UserInterface/nav.js
@@ -4,27 +4,33 @@ import { Customize } from './customize'
 export class Nav extends Component {
 
     state = {
-            spaceshipVisibility: 'hidden'
+            customize: false
     }
 
-    customizeSpaceship = () => {
-        this.setState({spaceshipVisibility: this.state.spaceshipVisibility == 'hidden'? 'visible' : 'hidden'})
+    customizeShip = () => {
+        this.setState({customize: this.state.customize == true ? false : true})
     }
 
     render() {
         return  <div>
                     <nav className="absolute bottom flex-col" >
-                        <button id="customize" className="btn" onClick = {this.customizeSpaceship}>
+                        <button className={"btn " + (this.state.customize ? '' : 'hidden')} onClick = {this.customizeShip}>
+                            <img src="/img/cancel.svg" alt="cancel" width="50px"/>
+                        </button>
+                        <button className={"btn " + (this.state.customize ? '' : 'hidden')} >
+                            <img src="/img/apply.svg" alt="apply" width="50px"/>
+                        </button>
+                        <button className={"btn " + (this.state.customize ? 'hidden' : '')} onClick = {this.customizeShip}>
                             <img src="/img/customize.svg" alt="customize" width="50px"/>
                         </button>
-                        <button id="navigate" className="btn">
+                        <button className={"btn " + (this.state.customize ? 'hidden' : '')}>
                             <img src="/img/navigate.svg" alt="navigate" width="50px"/>
                         </button>
-                        <button id="shop" className="btn">
+                        <button className={"btn " + (this.state.customize ? 'hidden' : '')}>
                             <img src="/img/shop.svg" alt="shop" width="50px"/>
                         </button>
                     </nav>
-                    <Customize spaceshipVisibility={this.state.spaceshipVisibility} session={this.props.session} products={this.props.products}/>
+                    <Customize customize={this.state.customize} session={this.props.session} products={this.props.products}/>
                 </div>
     }
 }

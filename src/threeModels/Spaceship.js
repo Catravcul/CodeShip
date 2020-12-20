31e8876b525e147bf6a77ade215617ed0e582f73
beron <bleink@outlook.es>
2020-12-20 22:42:19 +0100

    Spaceship.js - remove render from constructor, removed properties values by default

--- src/threeModels/Spaceship.js
+++ src/threeModels/Spaceship.js
@@ -2,22 +2,21 @@ import * as Components from './Components'
 
 export class Spaceship {
     spaceship
-    fuselage = new Components.BirdF()
-    takeoff = new Components.StandardTO(this.fuselage.positionTO, this.fuselage.rotationTO, this.fuselage.scaleTO)
-    propulsionEngine = new Components.BirdPE(this.fuselage.positionPE, this.fuselage.rotationPE)
+    fuselage
+    takeoff
+    propulsionEngine
 
     components = {fuselage: 'StandardF', takeoff: 'StandardTO', propulsionEngine: 'StandardPE'}
 
     constructor(p_spaceship) {
         this.spaceship = p_spaceship
-        this.renderComponents()
     }
 
     renderComponents() {
         this.spaceship.clear()
-        this.fuselage = new Components[this.components['fuselage']]()
-        this.takeoff = new Components[this.components['takeoff']](this.fuselage.positionTO, this.fuselage.rotationTO, this.fuselage.scaleTO)
-        this.propulsionEngine = new Components[this.components['propulsionEngine']](this.fuselage.positionPE, this.fuselage.rotationPE)
+        this.fuselage = new Components[this.components.fuselage]()
+        this.takeoff = new Components[this.components.takeoff](this.fuselage.positionTO, this.fuselage.rotationTO, this.fuselage.scaleTO)
+        this.propulsionEngine = new Components[this.components.propulsionEngine](this.fuselage.positionPE, this.fuselage.rotationPE)
         this.fuselage.renderOnGroup(this.spaceship)
         this.takeoff.renderOnGroup(this.spaceship)
         this.propulsionEngine.renderOnGroup(this.spaceship)

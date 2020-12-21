2a5491a2a4f9029a61b1d00593f533ddddf7bee4
beron <bleink@outlook.es>
2020-12-21 12:34:22 +0100

    change server to use env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv/config')
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -8,4 +9,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000);
+app.listen(process.env.PORT);

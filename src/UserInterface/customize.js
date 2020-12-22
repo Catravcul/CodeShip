cf5bc5c3b5596f75a40c53004799b60da829f66a
beron <bleink@outlook.es>
2020-12-22 01:55:26 +0100

    customize.js - create selectComponent handler to change 3d model & textarea

--- src/UserInterface/customize.js
+++ src/UserInterface/customize.js
@@ -4,18 +4,25 @@ import './customize.css'
 
 export class Customize extends Config {
     state = {
-        shipClass: ''
+        shipClass: '',
+        customClass: ''
     }
 
     componentDidMount() {
         fetch(Config.config.codeshipApi.urlBase + 'spaceship.txt', {method: 'GET'})
-        .then(res => res.text()).then( data => this.setState({shipClass: data}))
+        .then(res => res.text()).then( data => this.setState({customClass: data}, this.setState({shipClass: data})))
+    }
+
+    selectComponent = (component) => {
+        const word = 'Components.' + (component.type === 'fuselage' ? 'F' : component.type === 'takeoff' ? 'TO' : 'PE')
+        Config.shipInstance.setComponent(component.type, component.title)
+        this.setState({customClass: this.state.shipClass.replace(word, 'Components.' + component.title)})
     }
 
     render() {
         return  <div id="spaceship-class" className={ "customize-s screen " + (this.props.customize ? '' : 'hidden') }>
-                    <textarea value = { this.state.shipClass } className = "absolute ship-class" disabled ></textarea>
-                    <Carrousel session={this.props.session} products={this.props.products}/>
+                    <textarea value = { this.state.customClass } className = "absolute ship-class" disabled ></textarea>
+                    <Carrousel session={this.props.session} products={this.props.products} selectComponent={this.selectComponent}/>
                 </div>
     }
 }

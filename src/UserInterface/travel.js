275931526cb47d4751b78c08028a6c598667a312
beron <bleink@outlook.es>
2020-12-27 21:47:22 +0100

    travel.js - add button along with event to move spaceship forward

--- src/UserInterface/travel.js
+++ src/UserInterface/travel.js
@@ -5,13 +5,25 @@ export class Nav extends Config {
     exit = () => {
         this.props.shipTravel()
     }
+
+    move = amount => {
+        Config.shipInstance.propulsionEngine.run(Config.shipInstance.spaceship, 2)
+        Config.shipInstance.propulsionEngine.charge(Config.shipInstance.fuselage.energy)
+    }
+
     render() {
         return(
             <>
             <button className={"btn " + (this.props.travel ? '' : 'hidden')} onClick = {this.exit}>
             <img src="/img/cancel.svg" alt="cancel" width="50px"/>
             </button>
+            <button className={"btn " + (this.props.travel ? '' : 'hidden')} onClick = {this.move}>
+            <img src="/img/cancel.svg" alt="move" width="50px"/>
+            </button>
             </>
         )
     }
+}
+
+export class Interface extends Config {
 }

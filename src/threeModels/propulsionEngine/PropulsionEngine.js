65d95e9e7dfa9c3e3e849cca74d1e9746abadf4c
beron <bleink@outlook.es>
2020-12-27 22:31:14 +0100

    PropulsionEngine.js - run func makes movement interval, movement passed to accelerate func

--- src/threeModels/propulsionEngine/PropulsionEngine.js
+++ src/threeModels/propulsionEngine/PropulsionEngine.js
@@ -2,6 +2,7 @@ import { ThreeModel } from '../ThreeModel'
 
 export class PropulsionEngine extends ThreeModel {
     static folderPath = '3d/propulsion_engine/'
+    runInterval
     position
     rotation
     middlewares = {
@@ -18,23 +19,35 @@ export class PropulsionEngine extends ThreeModel {
     }
 
     /**
-     * Move spaceship forward
+     * Reset movement interval
      * @param {scene} spaceship
      * @param {int} speed
      */
     run(spaceship, speed) {
-        speed *= 0.1
+        clearInterval(this.runInterval)
         if (speed <= this.energy) {
-            this.energy -= speed
-            spaceship.translateZ(speed)
+            this.runInterval = setInterval(() => this.accelerate(spaceship, speed), 50)
         }
         console.log(speed)
     }
 
+    /**
+     * Move spaceship
+     * @param {scene} spaceship 
+     * @param {int} speed 
+     */
+    accelerate(spaceship, speed) {
+        spaceship.translateZ(speed)
+        speed *= 0.1
+        if (speed > this.energy) {
+            clearInterval(this.runInterval)
+        }
+        this.energy -= speed
+    }
     
     charge(energy) {
         if (this.energy !== this.potential) {
-            this.energy += energy * 0.1
+            this.energy += energy
         }
         console.log(this.energy)
     }

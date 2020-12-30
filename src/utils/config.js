3ab251378854fa9023c22404cd2e774e40a743d8
beron <bleink@outlook.es>
2020-12-30 23:31:48 +0100

    config.js - add codeshipNet object with urlbase property

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,16 +2,28 @@ const mode = 'PROD'
 
 const development = {
     codeshipApi: {
-    urlBase : 'http://127.0.0.1:5000/'
-}}
+        urlBase: 'http://127.0.0.1:5000/'
+    }, 
+    codeshipNet: {
+        urlBase: 'http://127.0.0.1:7000'
+    }
+}
 const test = {
     codeshipApi: {
-    urlBase : 'http://127.0.0.1:5000/'
-}}
+        urlBase: 'http://127.0.0.1:5000/'
+    }, 
+    codeshipNet: {
+        urlBase: 'http://127.0.0.1:7000'
+    }
+}
 const production = {
     codeshipApi: {
-    urlBase : 'https://codeship-api.herokuapp.com/'
-}}
+        urlBase: 'https://codeship-api.herokuapp.com/'
+    }, 
+    codeshipNet: {
+        urlBase: 'https://codeship-net.herokuapp.com'
+    }
+}
     
 export function getConfig() {
     switch(mode) {

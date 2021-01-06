6b0200b07851e0f49c7661e0b570a4702f8c526c
beron <bleink@outlook.es>
2021-01-06 15:59:29 +0100

    config.js - add urlBase for file system

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,26 +2,35 @@ const mode = 'PROD'
 
 const development = {
     codeshipApi: {
-        urlBase: 'http://127.0.0.1:5000/'
+        urlBase: 'http://127.0.0.1:5000'
     }, 
     codeshipNet: {
+        urlBase: 'http://127.0.0.1:3000'
+    },
+    codeshipFS: {
         urlBase: 'http://127.0.0.1:7000'
     }
 }
 const test = {
     codeshipApi: {
-        urlBase: 'http://127.0.0.1:5000/'
+        urlBase: 'http://127.0.0.1:5000'
     }, 
     codeshipNet: {
+        urlBase: 'http://127.0.0.1:3000'
+    },
+    codeshipFS: {
         urlBase: 'http://127.0.0.1:7000'
     }
 }
 const production = {
     codeshipApi: {
-        urlBase: 'https://codeship-api.herokuapp.com/'
+        urlBase: 'https://codeship-api.herokuapp.com'
     }, 
     codeshipNet: {
         urlBase: 'https://codeship-net.herokuapp.com'
+    },
+    codeshipFS: {
+        urlBase: 'https://codeship-fs.herokuapp.com'
     }
 }
     

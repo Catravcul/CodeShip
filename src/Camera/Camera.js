9af09566aa6ecd74dcb28c843b2d712cd2f76057
beron <bleink@outlook.es>
2021-03-11 23:50:06 +0100

    Camera.js - stores camera & orbitCamera as static properties, renders an h1 elements

---
+++ src/Camera/Camera.js
@@ -0,0 +1,14 @@
+import './Camera.css'
+
+export class Camera {
+    static camera
+    static orbitCamera
+
+    static render() {
+        return(
+        <div className='camera-c'>
+            <h1>Hola tu</h1>
+        </div>
+        )
+    }
+}

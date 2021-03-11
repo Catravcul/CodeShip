d1ea1fe39b32be0ef8c6b56a9ffc95186a69cb01
beron <bleink@outlook.es>
2021-03-11 23:54:49 +0100

    App.js - add Camera element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Game } from './Game'
+import { Camera } from './Camera/Camera'
 import { UserInterface } from './UserInterface/UserInterface'
 
 function App() {
   return (
     <div className="App">
       <Game />
+      <Camera.render />
       <UserInterface />
     </div>
   );

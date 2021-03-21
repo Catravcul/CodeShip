456b21a50ce9b106a797e42d16d82673e1668c2f
beron <bleink@outlook.es>
2021-03-21 19:07:18 +0100

    travel.js - hide speeds nav, add spin nav with classes for position and sizes

--- src/UserInterface/travel.js
+++ src/UserInterface/travel.js
@@ -50,10 +50,26 @@ export class Interface extends Config {
             }
         }
         return(
-            <nav className={"absolute bottom right speeds" + (this.props.travel ? '' : ' hidden')}>
-                {speeds.map(speed => (
-                    <input type="button" className="btn num" value={speed} onClick={this.changeSpeed}/>
-                ))}
+            // <nav className={"absolute bottom right speeds" + (this.props.travel ? '' : ' hidden')}>
+            //     {speeds.map(speed => (
+            //         <input type="button" className="btn num" value={speed} onClick={this.changeSpeed}/>
+            //     ))}
+            // </nav>
+            <nav className="absolute bottom right">
+                <input className="min-size-80-px size-8-vw rotate-90-deg" type="range" min="0" max="45"></input>
+                <div className="flex-row">
+                    <input className="min-size-80-px size-8-vw" type="range" min="0" max="45"></input>
+                    <div className="m-auto">
+                        <div className="min-w-80-px w-8-vw flex-row justify-content-space-around">
+                            <label>X: </label><input type="number" max="45" className="min-w-30-px w-1-vw"/>
+                        </div>
+                        <div className="min-w-80-px w-8-vw flex-row justify-content-space-around">
+                            <label>Y: </label><input type="number" max="45" className="min-w-30-px w-1-vw"/>
+                        </div>
+                    </div>
+                    <input className="min-size-80-px size-8-vw rotate-180-deg" type="range" min="0" max="45"></input>
+                </div>
+                <input className="min-size-80-px size-8-vw rotate-270-deg" type="range" min="0" max="45"></input>
             </nav>
         )
     }

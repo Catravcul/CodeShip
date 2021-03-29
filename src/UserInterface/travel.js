39ce317cd8e9d3bae3ec6b116423b2d3456931fb
beron <bleink@outlook.es>
2021-03-29 17:36:22 +0200

    travel.js - add style to inputs panel and range inputs of rotations nav

--- src/UserInterface/travel.js
+++ src/UserInterface/travel.js
@@ -56,20 +56,29 @@ export class Interface extends Config {
             //     ))}
             // </nav>
             <nav className="absolute bottom right">
-                <input className="min-size-80-px size-8-vw rotate-90-deg" type="range" min="0" max="45"></input>
+                <div className="min-size-80-px size-8-vw inline-block relative">
+                    <input className="w-100 absolute top-30 left rotate-90-deg glass-slider slider-thumb-turn-up" type="range" min="0" max="45"></input>
+                </div>
                 <div className="flex-row">
-                    <input className="min-size-80-px size-8-vw" type="range" min="0" max="45"></input>
-                    <div className="m-auto">
+                    <div className="min-size-80-px size-8-vw inline-block relative">
+                        <input className="w-100 absolute-vertical-center right-10 glass-slider slider-thumb-turn-left" type="range" min="0" max="45"></input>
+                    </div>
+                    <div className="m-auto color-white-and-black z-index-1">
                         <div className="min-w-80-px w-8-vw flex-row justify-content-space-around">
-                            <label>X: </label><input type="number" max="45" className="min-w-30-px w-1-vw"/>
+                            <label>X: </label><input type="number" min="-45" max="45" className="min-w-30-px w-1-vw"/>
                         </div>
                         <div className="min-w-80-px w-8-vw flex-row justify-content-space-around">
-                            <label>Y: </label><input type="number" max="45" className="min-w-30-px w-1-vw"/>
+                            <label>Y: </label><input type="number" min="-45" max="45" className="min-w-30-px w-1-vw"/>
                         </div>
                     </div>
-                    <input className="min-size-80-px size-8-vw rotate-180-deg" type="range" min="0" max="45"></input>
+                    <div className="min-size-80-px size-8-vw b-rad-30 absolute left-50 translateX-less-50 glass-panel"></div>
+                    <div className="min-size-80-px size-8-vw inline-block relative">
+                        <input className="w-100 absolute-vertical-center-flipped left-10 glass-slider slider-thumb-turn-right" type="range" min="0" max="45"></input>
+                    </div>
                 </div>
-                <input className="min-size-80-px size-8-vw rotate-270-deg" type="range" min="0" max="45"></input>
+                <div className="min-size-80-px size-8-vw inline-block relative">
+                    <input className="w-100 absolute bottom-30 left rotate-270-deg glass-slider slider-thumb-turn-down" type="range" min="0" max="45"></input>
+                </div>
             </nav>
         )
     }

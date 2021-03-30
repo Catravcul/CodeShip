c783d31321c3a9853f3700fc61179b7b20fe2b3a
beron <bleink@outlook.es>
2021-03-30 23:08:48 +0200

    travel.js - add spin function, and use it for onChange of range inputs to spin spaceship

--- src/UserInterface/travel.js
+++ src/UserInterface/travel.js
@@ -11,6 +11,7 @@ export class Nav extends Config {
         Config.shipInstance.propulsionEngine.run(Config.shipInstance.spaceship)
         Config.shipInstance.propulsionEngine.charge(Config.shipInstance.fuselage.energy)
     }
+
     stop = () => {
         Config.shipInstance.propulsionEngine.stop()
     }
@@ -41,6 +42,10 @@ export class Interface extends Config {
         Config.shipInstance.propulsionEngine.speed = e.currentTarget.value
         console.log(Config.shipInstance.propulsionEngine.speed)
     }
+    
+    spin = ({x, y, z}) => {
+        Config.shipInstance.propulsionEngine.spin(Config.shipInstance.spaceship, {x ,y ,z})
+    }
 
     render() {
         const speeds = []
@@ -60,11 +65,11 @@ export class Interface extends Config {
             :
             <nav className="absolute bottom right">
                 <div className="min-size-80-px size-8-vw inline-block relative">
-                    <input className="w-100 absolute top-30 left rotate-90-deg glass-slider slider-thumb-turn-up" type="range" min="0" max="45"></input>
+                    <input className="w-100 absolute top-30 left rotate-90-deg glass-slider slider-thumb-turn-up" type="range" onChange={e => this.spin({x:(-0.01 * e.currentTarget.value), y:0, z:0})} min="0" max="45"></input>
                 </div>
                 <div className="flex-row">
                     <div className="min-size-80-px size-8-vw inline-block relative">
-                        <input className="w-100 absolute-vertical-center right-10 glass-slider slider-thumb-turn-left" type="range" min="0" max="45"></input>
+                        <input className="w-100 absolute-vertical-center right-10 glass-slider slider-thumb-turn-left" type="range" onChange={e => this.spin({x:0, y:(-0.01 * e.currentTarget.value), z:0})} min="0" max="45"></input>
                     </div>
                     <div className="m-auto color-white-and-black z-index-1">
                         <div className="min-w-80-px w-8-vw flex-row justify-content-space-around">
@@ -76,11 +81,11 @@ export class Interface extends Config {
                     </div>
                     <div className="min-size-80-px size-8-vw b-rad-30 absolute left-50 translateX-less-50 glass-panel"></div>
                     <div className="min-size-80-px size-8-vw inline-block relative">
-                        <input className="w-100 absolute-vertical-center-flipped left-10 glass-slider slider-thumb-turn-right" type="range" min="0" max="45"></input>
+                        <input className="w-100 absolute-vertical-center-flipped left-10 glass-slider slider-thumb-turn-right" type="range" onChange={e => this.spin({x:0, y:(0.01 * e.currentTarget.value), z:0})} min="0" max="45"></input>
                     </div>
                 </div>
                 <div className="min-size-80-px size-8-vw inline-block relative">
-                    <input className="w-100 absolute bottom-30 left rotate-270-deg glass-slider slider-thumb-turn-down" type="range" min="0" max="45"></input>
+                    <input className="w-100 absolute bottom-30 left rotate-270-deg glass-slider slider-thumb-turn-down" type="range" onChange={e => this.spin({x:(0.01 * e.currentTarget.value), y:0, z:0})} min="0" max="45"></input>
                 </div>
             </nav>
         return(

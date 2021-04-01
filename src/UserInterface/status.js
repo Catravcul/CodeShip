0be3952b1641878ce91a58ecd62b025e94f3613b
beron <bleink@outlook.es>
2021-04-01 14:17:10 +0200

    status.js - create file with energy bar css and html

---
+++ src/UserInterface/status.js
@@ -0,0 +1,17 @@
+import { Config } from '../Config'
+
+export class Status extends Config {
+    radialColors = ['white', 'cyan', 'blue', 'lime', 'yellow', 'red', 'magenta', 'purple', 'brown', 'black']
+    radialGradient = 'radial-gradient(circle at left, ' + this.radialColors.join(',') + ')'
+    barStyle = {backgroundImage: this.radialGradient}
+    barClass = 'px-20-px py-3-px m-3-px b-rad-3-px bg-gray color-white-and-black '
+    render() {
+        return(
+            <>
+            <ul className='absolute bottom right list-style-none'>
+                <li className={this.barClass} style={this.barStyle}>Energy 100%</li>
+            </ul>
+            </>
+        )
+    }
+}

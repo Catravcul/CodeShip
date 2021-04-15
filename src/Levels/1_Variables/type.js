a7e991902b715ab0983755bccb3ee1b5014c8ed7
beron <bleink@outlook.es>
2021-04-15 02:28:04 +0200

    variables/type.js - export quest obj with info in spanish and english

---
+++ src/Levels/1_Variables/type.js
@@ -0,0 +1,26 @@
+
+
+export const quest = {
+    english: {
+        title: 'Variable types',
+        phrase: 'Some values seems to never change, others can change in any context while others in specific context',
+        intro: 'We would store some values pending on their posibility to change in future',
+        steps: [
+            'Approach the spaceship to a structure until appears code button (<>), then click it!',
+            'Drag and drop the code elements in correct order to create a javascript variable.'
+        ],
+        code: 'const goodWithOthers = true',
+        congratulate: 'Well done! it is time to pass to numbers.'
+    },
+    spanish: {
+        title: 'Tipos de variables',
+        phrase: 'Algunos valores parece que nunca cambian, otros pueden hacerlo en cualquier contexto, mientras otros en un contexto específico',
+        intro: 'Vamos a guardar algunos valores dependiendo de su posibilidad de cambiar en un futuro',
+        steps: [
+            'Acercate a la nave espacial hasta que aparezca el botón de código (<>), luego ¡dale un clic!.',
+            'Arrastra los elementos de código en el orden correcto para crear una variable de javascript.'
+        ],
+        code: 'const buenoConOtros = true',
+        congratulate: '¡Bien hecho! es hora de pasar a los números.'
+    }
+}

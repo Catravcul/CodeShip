1ddc13c020c9385a20bee997db5ff1428a55da3f
beron <bleink@outlook.es>
2021-04-16 01:59:20 +0200

    variables/type.js - change code per array, import codeSpans.js to display colored code

--- src/Levels/1_Variables/type.js
+++ src/Levels/1_Variables/type.js
@@ -1,26 +1,38 @@
 
+import {getBlueCode, getOrangeredCode, getWhiteboneCode, getLightblueCode} from '../codeSpans'
+import example from './type.jpg'
+
 
 export const quest = {
+    img: example,
     english: {
-        title: 'Variable types',
+        title: 'Javascript variable types',
         phrase: 'Some values seems to never change, others can change in any context while others in specific context',
         intro: 'We would store some values pending on their posibility to change in future',
         steps: [
             'Approach the spaceship to a structure until appears code button (<>), then click it!',
             'Drag and drop the code elements in correct order to create a javascript variable.'
         ],
-        code: 'const goodWithOthers = true',
+        code: [
+            <>{getBlueCode('const')}&nbsp;{getLightblueCode('goodWithOthers')}&nbsp;=&nbsp;{getBlueCode('true')}</>,
+            <>{getBlueCode('let')}&nbsp;{getLightblueCode('hungry')}&nbsp;=&nbsp;{getOrangeredCode('"100%"')}</>,
+            <>{getBlueCode('var')}&nbsp;{getLightblueCode('names')}&nbsp;=&nbsp;{getWhiteboneCode('4')}</>
+        ],
         congratulate: 'Well done! it is time to pass to numbers.'
     },
     spanish: {
-        title: 'Tipos de variables',
+        title: 'Tipos de variables en javascript',
         phrase: 'Algunos valores parece que nunca cambian, otros pueden hacerlo en cualquier contexto, mientras otros en un contexto específico',
         intro: 'Vamos a guardar algunos valores dependiendo de su posibilidad de cambiar en un futuro',
         steps: [
-            'Acercate a la nave espacial hasta que aparezca el botón de código (<>), luego ¡dale un clic!.',
+            'Acerca la nave espacial a una edificación hasta que aparezca el botón de código (<>), luego ¡dale un clic!.',
             'Arrastra los elementos de código en el orden correcto para crear una variable de javascript.'
         ],
-        code: 'const buenoConOtros = true',
+        code: [
+            <>{getBlueCode('const')}&nbsp;{getLightblueCode('buenoConOtros')}&nbsp;=&nbsp;{getBlueCode('true')}</>,
+            <>{getBlueCode('let')}&nbsp;{getLightblueCode('hambriento')}&nbsp;=&nbsp;{getOrangeredCode('"100%"')}</>,
+            <>{getBlueCode('var')}&nbsp;{getLightblueCode('nombres')}&nbsp;=&nbsp;{getWhiteboneCode('4')}</>
+        ],
         congratulate: '¡Bien hecho! es hora de pasar a los números.'
     }
 }

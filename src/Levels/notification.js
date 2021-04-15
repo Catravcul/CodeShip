aea95dfbb6ec25c8488c8a00d4e8143e2d4d28f1
beron <bleink@outlook.es>
2021-04-16 01:55:54 +0200

    notification.[js, css] - structure and style, to show quest with possibility of change language

--- src/Levels/notification.js
+++ src/Levels/notification.js
@@ -1,22 +1,41 @@
 import {useState, useCallback, useRef, memo} from 'react'
+import './notification.css'
 
-export const Notification = memo(({title, text}) => {
+export const Notification = memo(({quest}) => {
     
-    const [count, setCount] = useState(0)
-    const [showModal, setShowModal] = useState(true)
-    const renders = useRef(0)
+    const [showModal, setShowModal] = useState(false)
+    const [languaje, setLanguaje] = useState('english')
     const toggleModal = useCallback(() => setShowModal(prevState => prevState ? false : true), [])
+    const changeLanguaje = useCallback(lang => setLanguaje(() => lang), [])
     const getModal = useCallback(() =>
-        <article className='absolute'>
+        <article className='w-60-vw h-70-vh max-w-400-px max-h-550-px b-rad-10-px absolute middle color-white notification-c'>
             <header>
-                <button onClick={toggleModal}>x</button>
+                <button className='close' onClick={toggleModal}>x</button>
             </header>
-            <h1 style={{color:'white'}}>{title + renders.current}</h1>
-            <p>{text}</p>
-            <button onClick={()=>setCount(count+1)}>count {count}</button>
+            <h1>{quest[languaje].title}</h1>
+            <img src={quest.img} width='100%'/>
+            <nav className='w-100 flex-row p-unset justify-content-center'>
+                <ul className='languaje-select flex-row p-unset list-style-none py-3-px'>
+                    <li>
+                        <button className={'px-20-px py-3-px ' + (languaje == 'spanish' ? 'selected' : '')} onClick={() => changeLanguaje('spanish')}>Spanish</button>
+                    </li>
+                    <li>
+                        <button className={'px-20-px py-3-px ' + (languaje == 'english' ? 'selected' : '')} onClick={() => changeLanguaje('english')}>English</button>
+                    </li>
+                </ul>
+            </nav>
+            <h5>{quest[languaje].phrase}</h5>
+            <p>{quest[languaje].intro}</p>
+            <h2>Steps</h2>
+            <ol>
+                {quest[languaje].steps.map(step => <li>{step}</li>)}
+            </ol>
+            <h3>Examples</h3>
+            <ul>
+                {quest[languaje].code.map(code => <li><code>{code}</code></li>)}
+            </ul>
         </article>
-    , [])
-    
+    , [languaje])
     return(
         <>
         <button onClick={toggleModal}>Quest</button>

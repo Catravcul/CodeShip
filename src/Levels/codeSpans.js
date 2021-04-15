fcca730c9969b66feb841ce15d969bb1975633a4
beron <bleink@outlook.es>
2021-04-16 01:57:15 +0200

    codeSpans.js - add blue, ligthblue, orangered and whitebone

---
+++ src/Levels/codeSpans.js
@@ -0,0 +1,4 @@
+export const getBlueCode = code => <span className='color-blue'>{code}</span>
+export const getLightblueCode = code => <span className='color-lightblue'>{code}</span>
+export const getOrangeredCode = code => <span className='color-orangered'>{code}</span>
+export const getWhiteboneCode = code => <span className='color-whitebone'>{code}</span>

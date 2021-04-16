b599dd3447e587d0d5d3decca987bdbdd81a0b5b
beron <bleink@outlook.es>
2021-04-16 02:10:22 +0200

    status.js - fix class & make percentage number changes with props

--- src/UserInterface/status.js
+++ src/UserInterface/status.js
@@ -1,16 +1,17 @@
-import { Config } from '../Config'
+import {Config} from '../Config'
+
+const radialColors = ['white', 'cyan', 'blue', 'lime', 'yellow', 'red', 'magenta', 'purple', 'brown', 'black']
+const barClass = 'px-20-px py-3-px m-3-px b-rad-10-px bg-gray color-white-and-black '
 
 export class Status extends Config {
     render() {
         const energy = this.props.energyPercent/10
-        const radialColors = ['white', 'cyan', 'blue', 'lime', 'yellow', 'red', 'magenta', 'purple', 'brown', 'black']
         const radialGradient = 'radial-gradient(circle at left, ' + radialColors.slice(0, energy).join(',') + ', transparent ' + this.props.energyPercent + '%)'
         const barStyle = {backgroundImage: radialGradient}
-        const barClass = 'px-20-px py-3-px m-3-px b-rad-3-px bg-gray color-white-and-black '
         return(
             <>
             <ul className='absolute bottom right list-style-none'>
-                <li className={barClass} style={barStyle}>Energy 100%</li>
+                <li className={barClass} style={barStyle}>Energy {Math.round(this.props.energyPercent)}%</li>
             </ul>
             </>
         )

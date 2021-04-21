b39eda552a8fcc40375be140f9a37919f0ac227e
beron <bleink@outlook.es>
2021-04-21 19:24:38 +0200

    levels, variables - add codeModal and render first codeObject from sublevel file

--- src/Levels/1_Variables/index.js
+++ src/Levels/1_Variables/index.js
@@ -1,13 +1,47 @@
-import {memo, useCallback, useRef, useState} from 'react'
+import {memo, useMemo, useCallback, useRef, useState, useEffect, useLayoutEffect} from 'react'
+import {GltfLoader} from '../../utils/GltfLoader'
 import * as type from './type'
 import * as number from './number'
+import { CodeModal } from '../codeModal'
 
-export const Variables = memo(({Notification, levelUp}) => {
+export const Variables = memo(({Notification, levelUp, scene}) => {
     const subLevels = useRef({type, number})
     const [subLevel, setSubLevel] = useState('type')
     const levelsName = useRef(['type', 'number'])
     const changeLevel = useCallback(levelName => {setSubLevel(() => levelName)}, [])
+
+    const [indexScene, setIndexScene] = useState(-1)
+    const codeObjects = useRef([])
+    const threeScenes = useRef([])
+    const codeObject = useMemo(() => codeObjects.current[indexScene], [indexScene])
+    
+    const actualSublevel = subLevels.current[subLevel]
+    useEffect(() => {
+        actualSublevel.codeObjects.map(codeObj => {
+            GltfLoader.loadInArray(codeObj.modelFile, threeScenes.current, () => codeObjects.current.push(codeObj))
+        })
+        const startSublevel = () => setTimeout(() => {
+            if (codeObjects.current.length > 0) {
+                setIndexScene(() => {
+                    scene.add(threeScenes.current[0])
+                    threeScenes.current[0].translateZ(50)
+                    threeScenes.current[0].translateX(50)
+                    return 0
+                })
+            } else {
+                startSublevel()
+            }
+        }, 10000)
+        startSublevel()
+    }, [])
+    useLayoutEffect(() => {
+        scene.add(threeScenes.current[indexScene])
+    }, [indexScene])
+    
     return(
-        <Notification quest={subLevels.current[subLevel].quest} levelsName={levelsName} changeLevel={changeLevel} levelSelected={subLevel}/>
+        <>
+        <Notification quest={actualSublevel.quest} levelsName={levelsName} changeLevel={changeLevel} levelSelected={subLevel}/>
+        <CodeModal codeObject={codeObject}/>
+        </>
     )
 })
--- src/Levels/index.js
+++ src/Levels/index.js
@@ -1,9 +1,10 @@
 import {useState, useCallback} from 'react'
 import {Notification} from './notification'
+import {CodeModal} from './codeModal'
 
 import {Variables} from './1_Variables'
 
-export const Levels = () => {
+export const Levels = ({setlevelUpdates, scene}) => {
     const [levels, setLevels] = useState([Variables])
     const [level, setLevel] = useState(0)
     const levelUp = useCallback(() => setLevel(prevState => prevState < (levels.length-1) ? prevState + 1 : prevState))
@@ -11,7 +12,7 @@ export const Levels = () => {
 
     return(
         <div className='absolute'>
-            <Level Notification={Notification} levelUp={levelUp}/>
+            <Level Notification={Notification} CodeModal={CodeModal} levelUp={levelUp} setlevelUpdates={setlevelUpdates} scene={scene}/>
         </div>
     )
 

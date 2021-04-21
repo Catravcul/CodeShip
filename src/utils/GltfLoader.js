d4083b4863b481d7e9c993f048056d37e024831e
beron <bleink@outlook.es>
2021-04-21 19:21:34 +0200

    GltfLoader.js - add loadInArray func to create THREE scene and store it in an array

--- src/utils/GltfLoader.js
+++ src/utils/GltfLoader.js
@@ -4,11 +4,13 @@ export class GltfLoader{
     static loader = new GLTFLoader()
 
     /**
+     * 
      * Gives a 3d object already in scene a new child (3d object) which is loaded with GLTF
      * @param {THREE.Group} p_parent - 3d object in scene
      * @param {string} p_childPath - path of the .glb file
-     * @param {p_instance} p_instance - instance which propery is going to store rendered 3d model
-     * @param {string} property - property name
+     * @param {array} param2 - two elements array [object, string] 
+     * @param {object} middlewares - group of functions to execute with loaded 3d model scene as param
+     * @param {GltfLoader} loader - by default uses THREE GltfLoader
      */
     static loadAsChild(p_parent, p_childPath, [p_instance, p_property] = [{},'fail'], middlewares = {}, loader = GltfLoader.loader) {
         loader.load( p_childPath, function ( {scene} ) {
@@ -20,5 +22,19 @@ export class GltfLoader{
         }, undefined, function ( error ) {
             console.error( error );
         } );
+    }
+
+    /**
+     * 
+     * @param {string} p_path - path of .glb file
+     * @param {array} p_array - array where loaded THREE scene would be stored
+     * @param {function} callback - function to be called with loaded THREE scene as param
+     * @param {GltfLoader} loader - by default uses THREE GltfLoader
+     */
+    static loadInArray(p_path, p_array, callback = () => {}, loader = GltfLoader.loader) {
+        loader.load(p_path, function ( {scene} ) {
+            p_array.push(scene)
+            callback()
+        })
     }
 }

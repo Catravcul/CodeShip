8ab2c73a3962d8270140eba53a9b2d98228bf734
beron <bleink@outlook.es>
2021-04-21 18:50:18 +0200

    add codeSpans getCyanCode and move its styles to codeSpans.css from notification.css

--- src/Levels/codeSpans.js
+++ src/Levels/codeSpans.js
@@ -1,4 +1,7 @@
+import './codeSpans.css'
+
 export const getBlueCode = code => <span className='color-blue'>{code}</span>
 export const getLightblueCode = code => <span className='color-lightblue'>{code}</span>
 export const getOrangeredCode = code => <span className='color-orangered'>{code}</span>
 export const getWhiteboneCode = code => <span className='color-whitebone'>{code}</span>
+export const getCyanCode = code => <span className='color-cyan'>{code}</span>

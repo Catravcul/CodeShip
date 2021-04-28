39d00396d9e8d25d081266e091d6cf493270c663
beron <bleink@outlook.es>
2021-04-28 20:22:30 +0200

    index.js, notification.[js, css] - change subLevel variables for quest, make notification img show /hide bigger img by click

--- src/Levels/1_Variables/index.js
+++ src/Levels/1_Variables/index.js
@@ -5,24 +5,24 @@ import * as number from './number'
 import {CodeModal} from '../codeModal'
 
 export const Variables = memo(({Notification, levelUp, scene}) => {
-    const subLevels = useRef({type, number})
-    const [subLevel, setSubLevel] = useState('type')
-    const levelsName = useRef(['type', 'number'])
-    const changeLevel = useCallback(levelName => {setSubLevel(() => levelName)}, [])
+    const quests = useRef({type, number})
+    const [quest, setQuest] = useState('type')
+    const questsName = useRef(['type', 'number'])
+    const changeQuest = useCallback(levelName => {setQuest(() => levelName)}, [])
 
     const [indexScene, setIndexScene] = useState(-1)
     const codeObjects = useRef([])
     const threeScenes = useRef([])
     const codeObject = useMemo(() => codeObjects.current[indexScene], [indexScene])
 
-    const actualSublevel = subLevels.current[subLevel]
+    const actualQuest = quests.current[quest]
     const oldScene = threeScenes.current[indexScene]
     useEffect(() => {
         scene.remove(oldScene)
         codeObjects.current.length = 0
         threeScenes.current.length = 0
         setIndexScene(() => -1)
-        actualSublevel.codeObjects.map(codeObj => {
+        actualQuest.codeObjects.map(codeObj => {
             GltfLoader.loadInArray(codeObj.modelFile, threeScenes.current, () => codeObjects.current.push(codeObj))
         })
         const startSublevel = () => setTimeout(() => {
@@ -36,13 +36,23 @@ export const Variables = memo(({Notification, levelUp, scene}) => {
             } else {
                 startSublevel()
             }
-        }, 10000)
+        }, 5000)
         startSublevel()
-    }, [subLevel])
+        return scene.remove(oldScene)
+    }, [quest])
     
+    const getQuestsLi = useCallback(() => 
+        questsName.current.map( questName =>
+            <li id={questName}>
+                <button className={`px-20-px py-3-px ${(questName === quest ? 'selected' : '')}`} onClick={() => changeQuest(questName)}>
+                    {questName}
+                </button>
+            </li>
+        )
+    ,[quest])
     return(
         <>
-        <Notification quest={actualSublevel.quest} levelsName={levelsName} changeLevel={changeLevel} levelSelected={subLevel}/>
+        <Notification quest={actualQuest.quest} questSelected={quest} getQuestsLi={getQuestsLi}/>
         <CodeModal codeObject={codeObject}/>
         </>
     )
--- src/Levels/notification.js
+++ src/Levels/notification.js
@@ -1,19 +1,22 @@
 import {useState, useCallback, memo} from 'react'
 import './notification.css'
 
-export const Notification = memo(({quest, levelsName, changeLevel, levelSelected}) => {
+export const Notification = memo(({quest, getQuestsLi, questSelected}) => {
     
     const [showModal, setShowModal] = useState(false)
     const [languaje, setLanguaje] = useState('english')
     const toggleModal = useCallback(() => setShowModal(prevState => prevState ? false : true), [])
     const changeLanguaje = useCallback(lang => setLanguaje(() => lang), [])
+    const toggleImage = useCallback(() => {
+        document.getElementById('notification-img').classList.toggle('hide')
+    },[])
     const getModal = useCallback(() =>
         <article className='h-70-vh max-w-400-px max-h-550-px b-rad-10-px absolute middle color-white notification-c'>
             <header>
                 <button className='close' onClick={toggleModal}>x</button>
             </header>
             <h1>{'Javascript - ' + quest[languaje].title}</h1>
-            <img src={quest.img} width='100%' alt='Screenshot as example'/>
+            <img src={quest.img} width='100%' alt='Screenshot as example' onClick={toggleImage}/>
             <nav className='w-100 flex-row p-unset justify-content-center'>
                 <ul className='languaje-select flex-row p-unset list-style-none py-3-px'>
                     <li>
@@ -40,19 +43,14 @@ export const Notification = memo(({quest, levelsName, changeLevel, levelSelected
             </ul>
             <nav className='w-100 flex-row p-unset justify-content-center'>
                 <ul className='languaje-select flex-row p-unset list-style-none py-3-px'>
-                    {levelsName.current.map( levelName =>
-                        <li id={levelName}>
-                            <button className={'px-20-px py-3-px ' + (levelName === levelSelected ? 'selected' : '')} onClick={() => changeLevel(levelName)}>
-                                {levelName}
-                            </button>
-                        </li>
-                    )}
+                    {getQuestsLi()}
                 </ul>
             </nav>
         </article>
-    , [languaje, levelSelected])
+    , [languaje, questSelected])
     return(
         <>
+        <div id='notification-img' className='absolute left top quest-img big-img hide' style={{backgroundImage: `url(${quest.img})`}} onClick={toggleImage}></div>
         <button onClick={toggleModal}>Quest</button>
         {showModal ? getModal() : <></>}
         </>
